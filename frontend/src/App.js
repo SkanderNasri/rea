8144243f2f00513a7b9a4576e08c7c97198fff91
Skander Nasri <skandernasri1994@gmail.com>
2024-02-18 00:13:53 +0100

    Playing with Ant Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,54 @@
 import './App.css';
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {DatePicker} from 'antd';
+import { QuestionCircleOutlined, SyncOutlined } from '@ant-design/icons';
+import { FloatButton } from 'antd';
+import { Col, Row, Statistic } from 'antd';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+
+const { Countdown } = Statistic;
+const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Dayjs is also OK
+
+const { Header, Content, Footer, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+const onFinish = () => {
+  console.log('finished!');
+};
+const onChange = (val) => {
+  if (typeof val === 'number' && 4.95 * 1000 < val && val < 5 * 1000) {
+    console.log('changed!');
+  }
+};
 
 
 function App() {
@@ -24,18 +72,110 @@ function App() {
     setCounter(counter + 1);
   };
 
-  
-
-
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   return (
-    <>
-      <h1>My Website</h1>
+    <div >
+      <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <h1>My Website</h1>
       <h3>My Name is {userName}</h3>
       <button onClick={handleClick1}>+</button>
       <button onClick={() => setCounter(counter - 1)}>-</button>
       <h3>{counter}</h3>
-    </>
+      <DatePicker />
+      <FloatButton.Group
+      shape="circle"
+      style={{
+        right: 24,
+      }}
+    >
+      <FloatButton icon={<QuestionCircleOutlined />} />
+      <FloatButton />
+      <FloatButton.BackTop visibilityHeight={0} />
+    </FloatButton.Group>
+    <FloatButton.Group
+      shape="square"
+      style={{
+        right: 94,
+      }}
+    >
+      <FloatButton icon={<QuestionCircleOutlined />} />
+      <FloatButton />
+      <FloatButton icon={<SyncOutlined />} />
+      <FloatButton.BackTop visibilityHeight={0} />
+    </FloatButton.Group>
+
+    <Row gutter={16}>
+    <Col span={12}>
+      <Countdown title="Countdown" value={deadline} onFinish={onFinish} />
+    </Col>
+    <Col span={12}>
+      <Countdown title="Million Seconds" value={deadline} format="HH:mm:ss:SSS" />
+    </Col>
+    <Col
+      span={24}
+      style={{
+        marginTop: 32,
+      }}
+    >
+      <Countdown title="Day Level" value={deadline} format="HH:mm:ss:SSS" />
+    </Col>
+    <Col span={12}>
+      <Countdown title="Countdown" value={Date.now() + 10 * 1000} onChange={onChange} />
+    </Col>
+  </Row>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          ReDnaKs AG ©{new Date().getFullYear()} Created by Skander Nasri
+        </Footer>
+      </Layout>
+    </Layout>
+      
+    </div>
   );
 }
 
